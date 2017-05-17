76b5cbcba685993fd4efdc9c3f347bf6d702d52c
Jared Allard <jared@staymarta.com>
2017-05-17 14:19:42 -0700

    basic schema to check POST /

--- index.js
+++ index.js
@@ -10,8 +10,22 @@
 
 const debug         = require('debug')('staymarta:users')
 const express       = require('express')
+const Joi           = require('joi')
 
+const bodyP         = require('body-parser')
+const validate      = require('express-validation')
 const app           = express();
+
+// disable unknowns.
+const schemaOptions = {
+  allowUnknownBody:    false,
+  allowUnknownHeaders: false,
+  allowUnknownQuery:   false,
+  allowUnknownParams:  false,
+  allowUnknownCookies: false
+}
+
+app.use(bodyP.json())
 
 app.get('/me', (req, res) => {
   return res.send({
@@ -20,6 +34,21 @@ app.get('/me', (req, res) => {
     last_name: 'Allard',
     email: 'jaredallard@outlook.com'
   });
+})
+
+app.post('/',
+  validate({
+    options: schemaOptions,
+    body: {
+      username: Joi.string().alphanum().required(),
+      full_name: Joi.string().required(),
+      age: Joi.number().required(),
+      location: Joi.string().required(),
+      email: Joi.string().email().required()
+    }
+  }),
+(req, res) => {
+  return res.send(req.body)
 })
 
 app.listen(80);
